ecd3f62ecd71887d30e8d76a671bb440733ea145
Steven Ortega <stevenvortega@gmail.com>
2020-08-26 14:29:58 -0700

    json methods now included

--- Resources/js/script.js
+++ Resources/js/script.js
@@ -3,3 +3,15 @@ or for the buttons or when you scroll down the page*/
 
 /* some example js code*/
 window.alert(5 + 6);
+
+/* some example json code using the parse() method */
+var txt = '{"name":"John", "age":30, "city":"New York"}'
+var obj = JSON.parse(txt);
+document.getElementById("demo").innerHTML = obj.name + ", " + obj.age;
+// expected output: true
+
+
+/* some example json code using the string() method */
+var obj = { name: "John", age: 30, city: "New York" };
+var myJSON = JSON.stringify(obj);
+document.getElementById("demo2").innerHTML = myJSON;
