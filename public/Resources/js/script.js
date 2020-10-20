ec3da1491231cbab9ded99ec13ee3f8c605b8297
Steven Ortega <stevenvortega@gmail.com>
2020-10-20 12:52:24 -0700

    update to Index.html, css, and javascript

--- public/Resources/js/script.js
+++ public/Resources/js/script.js
@@ -1,41 +1,8 @@
-var slideIndex = 0;
-showSlides();
+document.addEventListener('DOMContentLoaded', function() {
+    var modals = document.querySelectorAll('.modal');
+    M.Modal.init(modals);
 
-function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
-}
+    var items = document.querySelectorAll('.collapsible');
+    M.Collapsible.init(items);
 
-
-
-
-/* might have some animations for the navigation
-or for the buttons or when you scroll down the page*/
-
-/* some example js code
-window.alert(5 + 6);
-
-/* some example json code using the parse() method
-var txt = '{"name":"John", "age":30, "city":"New York"}'
-var obj = JSON.parse(txt);
-document.getElementById("demo").innerHTML = obj.name + ", " + obj.age;
-// expected output: true
-
-
-/* some example json code using the string() method
-var obj = { name: "John", age: 30, city: "New York" };
-var myJSON = JSON.stringify(obj);
-document.getElementById("demo2").innerHTML = myJSON;
-*/
+});
