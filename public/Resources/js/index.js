476081dfe71c9f7df299c51f637b87c8f638dfe0
Steven Ortega <stevenvortega@gmail.com>
2020-10-27 18:07:28 -0700

    updating

---
+++ public/Resources/js/index.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    var modals = document.querySelectorAll('.modal');
+    M.Modal.init(modals);
+
+    var items = document.querySelectorAll('.collapsible');
+    M.Collapsible.init(items);
+})
