1719c48adeb538640eba01dfd3f8d1197a0644af
Steven Ortega <stevenvortega@gmail.com>
2020-11-02 15:30:38 -0800

    new updates

--- public/Resources/js/auth.js
+++ public/Resources/js/auth.js
@@ -1,18 +1,37 @@
 //get data
-db.collection('guides').get().then(snapshot => {
-    console.log(snapshot.docs)
-});
 
 
-
+//listen for auth status changes
 auth.onAuthStateChanged(user => {
     if(user){
-        console.log('user logged in', user);
+        db.collection('guides').onSnapshot(snapshot => {
+    setupGuides(snapshot.docs);
+    setupUI(user);
+});
     }
     else {
-        console.log('user logged out');
+        setupUI();
+        setupGuides([]);
     }
 });
+
+
+//create new guide
+const createForm = document.querySelector('#create-form');
+createForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    db.collection('guides').add({
+        title: createForm['title'].value,
+        content: createForm['content'].value
+    }).then(() => {
+        //close the modal and reset the form
+        const modal = document.querySelector('#modal-create');
+        M.Modal.getInstance(modal).close();
+        createForm.reset();
+    }).catch(err=> {
+        console.log(err.message);
+    })
+})
 
 
 //signup
@@ -26,12 +45,12 @@ signupForm.addEventListener('submit',(e) => {
 
 
     auth.createUserWithEmailAndPassword(email, password).then(cred => {
-        const modal = document.querySelector('#signup-form');
+        const modal = document.querySelector('#modal-signup');
         M.Modal.getInstance(modal).close();
         signupForm.reset();
-    });
+    })
 
-});
+})
 
 //logout
 const logout = document.querySelector('#logout');
